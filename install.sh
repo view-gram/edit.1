189ff3718e499236f312261852428d4efc4140ad
view-gram <error.3357@gmail.com>
2017-04-28 00:04:48 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
 
-sudo apt-get install lua-lgi
+sudo apt-get install lua-lgi -y
 
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common -y
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 
-sudo apt-get install libstdc++6
+sudo apt-get install libstdc++6 -y
 
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
-sudo apt-get dist-upgrade
+sudo apt-get dist-upgrade -y
 
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  tar zxpf luarocks-2.2.2.tar.gz
